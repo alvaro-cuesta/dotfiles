5052bc0b478a93f7325c975d0c42003247ed97af
alvaro-cuesta <alvaro-cuesta@GitHub>
2012-10-01 02:29:07 +0200

    Fixed Python not loading global config

--- python.sh
+++ python.sh
@@ -2,6 +2,6 @@
 git clone https://github.com/gabrielelanaro/emacs-for-python.git ~/.emacs.python.d
 cp ./pycheckers ~/.emacs.python.d/
 echo "#!/bin/bash" > ~/emacs-py.sh
-echo "emacs -q -l ~/.emacs.python.el" >> ~/emacs-py.sh
+echo "emacs -l ~/.emacs.python.el" >> ~/emacs-py.sh
 chmod +x ~/emacs-py.sh
 cp ./.emacs.python.el ~
