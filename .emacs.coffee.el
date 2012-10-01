e10e4137daffa5a567e54ba3fc2a4bc8468eabd9
alvaro-cuesta <alvaro-cuesta@GitHub>
2012-10-01 02:29:46 +0200

    Fixed Coffee config

--- .emacs.coffee.el
+++ .emacs.coffee.el
@@ -1,1 +1,4 @@
 (load-file "~/.emacs.coffee.d/coffee-mode.el")
+
+(custom-set-variables
+ '(coffee-tab-width 2))
--- coffee.sh
+++ coffee.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 mkdir ~/.emacs.coffee.d/
-wget https://raw.github.com/defunkt/coffee-mode/master/coffee-mode.el ~/.emacs.coffee.d/
+wget https://raw.github.com/defunkt/coffee-mode/master/coffee-mode.el
+mv ./coffee-mode.el ~/.emacs.coffee.d/
+echo "#!/bin/bash" > ~/emacs-coffee.sh
+echo "emacs -l ~/.emacs.coffee.el" >> ~/emacs-coffee.sh
+chmod +x ~/emacs-coffee.sh
+cp ./.emacs.coffee.el ~
